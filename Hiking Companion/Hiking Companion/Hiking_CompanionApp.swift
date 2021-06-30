75fec508855bd1f0c8f7b76e07a8b6a6065e25e0
Cédric Derache <cedric.derache@hotmail.fr>
2021-06-30 17:15:39 +0200

    Add Sentry

--- Hiking Companion/Hiking Companion/Hiking_CompanionApp.swift
+++ Hiking Companion/Hiking Companion/Hiking_CompanionApp.swift
@@ -5,6 +5,7 @@
 //  Created by Cédric Derache on 30/06/2021.
 //
 
+import Sentry
 import SwiftUI
 
 @main
@@ -12,6 +13,21 @@ struct Hiking_CompanionApp: App {
     var body: some Scene {
         WindowGroup {
             MainView()
+                .onAppear {
+                    SentrySDK.start { options in
+                        options.dsn = "https://eb6baf06b0be4da1b7423897940e7c4b@o896699.ingest.sentry.io/5841313"
+                        options.debug = false
+                        
+                        options.environment = "development"
+                        
+                        // Issues
+                        options.enableAutoSessionTracking = true
+                        options.enableOutOfMemoryTracking = true
+                        
+                        // Performance
+                        options.tracesSampleRate = 0.25
+                    }
+                }
         }
     }
 }
